707aab7c9d3019168b288b1a8ae146d77157c822
Son dang <sondn.da@gmail.com>
2020-08-28 15:37:19 +0700

    sidedrawer

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import AuxHoc from '../../hoc/AuxHoc';
 import classes from './Layout.module.css'
+import Toolbar from '../Navigation/Toolbar/Toolbar'
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer'
 
 const Layout = (props) => (
   <AuxHoc>
-    <div>Toolbar, Sidebar, Backdrop</div>
+    <Toolbar></Toolbar>
+    <SideDrawer />
     <main className={classes.mt16}>
       {props.children}
     </main>
---
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import classes from './Toolbar.module.css'
+import Logo from '../../Logo/Logo'
+import NavigationItems from '../NavigationItems/NavigationItems'
+
+const Toolbar = (props) => (
+  <header className={classes.Toolbar}>
+    <div>Menu</div>
+    <Logo height/>
+    <nav>
+      <NavigationItems />
+    </nav>
+  </header>
+)
+export default Toolbar;
